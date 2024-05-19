ef547a0f3338fe32be1190be16042bde124c6245
vikimova <wikimova@gmail.com>
2024-05-19 16:48:46 +0300

    test

--- pacman.c
+++ pacman.c
@@ -3,7 +3,7 @@
 void init_game() {
    for(int i = 0; i < monster_count; ++i) {
        monsters[i].id = i + 1;
-       monsters[i].role = 1;
+       monsters[i].role = 3;
       //...
    }
    player.id = 0; 
